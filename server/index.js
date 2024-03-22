c81f915e82e4828577ca3210aa4f7b83ba464568
Siddhant51 <124143317+Siddhant51@users.noreply.github.com>
2024-03-22 21:51:20 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -7,7 +7,7 @@ require("dotenv").config();
 
 // Configure CORS more specifically
 const corsOptions = {
-  origin: "http://localhost:3000", // Replace with your client-side URL
+  origin: "https://tic-tac-toe-socket-sid.vercel.app", // Replace with your client-side URL
   methods: ["GET", "POST"],
 };
 app.use(cors(corsOptions));
