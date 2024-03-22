d53ec36577774350a98c744b6083f63c319cd3c9
Siddhant51 <124143317+Siddhant51@users.noreply.github.com>
2024-03-22 21:52:45 +0530

    Update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -3,7 +3,7 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Lobby from "./pages/Lobby";
 import GameBoard from "./pages/GameBoard";
 import io from "socket.io-client";
-const socket = io.connect("http://localhost:3001");
+const socket = io.connect("https://tic-tac-toe-socket-api.vercel.app/");
 
 function App() {
   return (
