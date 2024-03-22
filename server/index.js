77b330a87d7eb5707dad28b58082f9a34b8add98
Siddhant51 <124143317+Siddhant51@users.noreply.github.com>
2024-03-22 22:01:16 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -40,7 +40,7 @@ const server = http.createServer(app);
 
 const io = new Server(server, {
   cors: {
-    origin: "http://localhost:3000", // Replace with your client-side URL
+    origin: "https://tic-tac-toe-socket-sid.vercel.app/", // Replace with your client-side URL
     methods: ["GET", "POST"],
   },
 });
